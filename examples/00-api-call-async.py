e1dfc662420ce29f4e09607281ba1839b5130838
Harshad Sharma <harshad@sharma.io>
2024-10-17 21:07:03 +0530

    Update examples to show passing options in API call

--- examples/00-api-call-async.py
+++ examples/00-api-call-async.py
@@ -7,8 +7,14 @@ from logspyq import LogseqAPI
 
 logseq = LogseqAPI(auth_token="test")
 
+
 async def main():
-    await logseq.call("logseq.UI.showMsg", "Hello from Python!")
+    await logseq.call(
+        "logseq.UI.showMsg",
+        "Hello from Python!",
+        "success",
+        {"key": "unique-key", "timeout": 5000},
+    )
 
 
 asyncio.run(main())
--- examples/00-api-call.py
+++ examples/00-api-call.py
@@ -1,8 +1,14 @@
 """
 Use Logseq REST API to display notification in the Logseq desktop app.
 """
+
 from logspyq import LogseqAPI
 
 logseq = LogseqAPI(auth_token="test")
 
-logseq.call("logseq.UI.showMsg", "Hello from Python!")
+logseq.call(
+    "logseq.UI.showMsg",
+    "Hello from Python!",
+    "success",
+    {"key": "unique-key", "timeout": 5000},
+)
